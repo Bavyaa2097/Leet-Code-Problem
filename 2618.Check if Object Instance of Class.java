9a530d240607c40a3b5854f028f6f500b2a69d06
Bavyaa2097 <bavyaa2097@gmail.com>
2026-07-27 12:59:19 +0530

    Add function to check object instance of class

---
+++ 2618.Check if Object Instance of Class.java
@@ -0,0 +1,14 @@
+var checkIfInstanceOf = function (obj, classFunction) {
+    if (obj == null || typeof classFunction !== "function") return false;
+
+    let proto = Object.getPrototypeOf(obj);
+
+    while (proto) {
+        if (proto === classFunction.prototype) {
+            return true;
+        }
+        proto = Object.getPrototypeOf(proto);
+    }
+
+    return false;
+};
